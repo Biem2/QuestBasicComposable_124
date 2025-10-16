9158f789f7c11b9ab8ab9e5ff1bf10ccf8050a7d
Biem2 <abiem.muhammad.ft23@mail.umy.ac.id>
2025-10-16 16:47:40 +0700

    Menambahkan composable LatelatakRowColumn dengan layout Row berisi dua Column

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -90,3 +90,20 @@ fun LatelatakColumnRow(modifier: Modifier) {
         }
     }
 }
+@Composable
+fun LatelatakRowColumn(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        //Kolom1
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
+
+        //Kolom2
+        Column() {
+            Text(text = "Komponen1Kolom2")
+            Text(text = "Komponen2Kolom2")
+        }
+    }
+}
